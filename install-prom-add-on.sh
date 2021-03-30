4dbbf5921c2bd5b99b0f3c794e0ba8aca71777e6
Srinivasan Parthasarathy <spartha@us.ibm.com>
2021-03-29 23:00:56 -0400

    prom addon (#40)

---
+++ install-prom-add-on.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Step 0: Export TAG
+export TAG="${TAG:-v0.3.0-pre.4}"
+
+# Step 1: Install Prometheus add-on
+# This step assumes you have installed Iter8 using install.sh
+echo "Installing Prometheus add-on"
+kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/prometheus-add-on/prometheus-operator/build.yaml
+kubectl wait crd -l creator=iter8 --for condition=established --timeout=120s
+kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/prometheus-add-on/prometheus/build.yaml
+kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/prometheus-add-on/service-monitors/build.yaml
+
+echo "Verifying Prometheus-addon installation"
+kubectl wait --for condition=ready --timeout=300s pods --all -n iter8-system
+
+set +e
+
+return 0
